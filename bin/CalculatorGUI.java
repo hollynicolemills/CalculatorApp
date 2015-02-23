2f5da85a34df91b5f525259e39ecb2319777261f
Holly Mills <hollynicolemills@gmail.com>
2015-02-22 20:03:24 -0500

    Update CalculatorGUI.java

--- bin/CalculatorGUI.java
+++ bin/CalculatorGUI.java
@@ -1,11 +1,10 @@
 /** 
 Author: Holly Mills
 Creation Date: Feb. 14, 2015
-Last Edited: Feb. 16, 2015
-Description: Ver. 3 uses CreateButton class and Calculator class to simplify code and
+Last Edited: Feb. 22, 2015
+Description: Ver. 4 uses CreateButton class and OrderOfOperations class to simplify code and
 		also uses getActionEvent() to identify button being pressed
-		can accept multiple inputs
-		still doesn't know order of operations
+		Can accept multiple inputs and uses order of operations to calculate currect answer
 
  CalculatorApp
     Copyright (C) 2015  Holly Mills
@@ -36,18 +35,18 @@ public class CalculatorGUI {
     CreateButton one, two, three, four, five, six, seven, 
 	eight, nine, dot, zero, addition, subtract,
 	multiply, divide, equals, clear,
-	sine, cosine, tangent, sqrt;
-	 	
-    Calculator evaluator;
-
-    JTextField textBox, displayBox;
+    	sqrt, close, open, exp;
+    
+    OrderOfOperations evaluator;
 	
-    double nextNum = 0.0;
+    JTextField displayBox;
+	
+    double num = 0.0;
     double result = 0.0;
-    boolean first = true;
-    char assign = 'n';
+    boolean squareroot = false;
     String numHolder = "0";
-    String button = ""; 
+    String button = "";
+    String input = "";
     
     DecimalFormat formater = new DecimalFormat("0.##########");
 	
@@ -59,114 +58,94 @@ public class CalculatorGUI {
     public void go() {
 	JFrame frame = new JFrame("Calculator");
 	JPanel centerPane = new JPanel();
-	JPanel leftPane = new JPanel();
 	JPanel bottomPane = new JPanel();
-		
-	//Set Layouts 
-	centerPane.setLayout(new GridLayout(4,4,2,2));
-	leftPane.setLayout( new GridLayout(4,2,2,2));
+		 
+	centerPane.setLayout(new GridLayout(5,4));
 	bottomPane.setLayout( new GridLayout(1,1,2,2));
 		
-	//Create TextBox
 	displayBox = new JTextField(20);
-		
-	evaluator = new Calculator();
+	evaluator = new OrderOfOperations();
 
-	one = new CreateButton("1", new NumberButton(), centerPane);
-	two = new CreateButton("2", new NumberButton(), centerPane);
-	three = new CreateButton("3", new NumberButton(), centerPane);
-	four = new CreateButton("4", new NumberButton(), centerPane);
-	five = new CreateButton("5", new NumberButton(), centerPane);
-	six = new CreateButton("6", new NumberButton(), centerPane);
-	seven = new CreateButton("7", new NumberButton(), centerPane);
-	eight = new CreateButton("8", new NumberButton(), centerPane);
-	nine = new CreateButton("9", new NumberButton(), centerPane);
-	dot = new CreateButton(".", new NumberButton(), centerPane);
+    	sqrt = new CreateButton("sqrt", new OperationButton(), centerPane);
+    	exp = new CreateButton("^", new OperationButton(), centerPane);
+    	open = new CreateButton("(", new OperationButton(), centerPane);
+    	close = new CreateButton(")", new OperationButton(), centerPane);
+    	seven = new CreateButton("7", new NumberButton(), centerPane);
+    	eight = new CreateButton("8", new NumberButton(), centerPane);
+    	nine = new CreateButton("9", new NumberButton(), centerPane);
+    	addition = new CreateButton(" + ", new OperationButton(), centerPane);
+    	four = new CreateButton("4", new NumberButton(), centerPane);
+    	five = new CreateButton("5", new NumberButton(), centerPane);
+    	six = new CreateButton("6", new NumberButton(), centerPane);
+    	subtract = new CreateButton(" - ", new OperationButton(), centerPane);
+    	one = new CreateButton("1", new NumberButton(), centerPane);
+    	two = new CreateButton("2", new NumberButton(), centerPane);
+    	three = new CreateButton("3", new NumberButton(), centerPane);
+    	multiply = new CreateButton(" * ", new OperationButton(), centerPane);
+    	dot = new CreateButton(".", new NumberButton(), centerPane);
 	zero = new CreateButton("0", new NumberButton(), centerPane);
-	equals = new CreateButton(" = ", new EqualsButton(), centerPane);
-	addition = new CreateButton(" + ", new OperationButton(), leftPane);
-	sine = new CreateButton("sin(x)", new OperationButton(), leftPane);
-	subtract = new CreateButton(" - ", new OperationButton(), leftPane);
-	cosine = new CreateButton("cos(x)", new OperationButton(), leftPane);
-	multiply = new CreateButton(" * ", new OperationButton(), leftPane);
-	tangent = new CreateButton("tan(x)", new OperationButton(), leftPane);
-	divide = new CreateButton("/", new OperationButton(), leftPane);
-	sqrt = new CreateButton("sqrt", new OperationButton(), leftPane);
-	clear = new CreateButton("clear", new ClearButton(), bottomPane);
+    	equals = new CreateButton(" = ", new EqualsButton(), centerPane);
+    	divide = new CreateButton(" / ", new OperationButton(), centerPane);
+	clear = new CreateButton("clear", new OperationButton(), bottomPane);
 		
-	//Create layout
 	frame.getContentPane().add(BorderLayout.NORTH, displayBox);
 	frame.getContentPane().add(BorderLayout.CENTER, centerPane);
-	frame.getContentPane().add(BorderLayout.EAST, leftPane);
 	frame.getContentPane().add(BorderLayout.SOUTH, bottomPane);
 		
-	//Set sizes and defaults
-	frame.setSize(350,225);
+	frame.setSize(250,300);
 	frame.setVisible(true);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 		
     class NumberButton implements ActionListener {		
-	public void actionPerformed(ActionEvent event) {
-	    button = event.getActionCommand();
-	    numHolder = numHolder + button;
-	    displayBox.setText(displayBox.getText() + button);
-	}
+        public void actionPerformed(ActionEvent event) {
+            button = event.getActionCommand();
+            numHolder = numHolder + button;
+            displayBox.setText(displayBox.getText() + button);
+            if (!squareroot)
+                input = input + button;
+        }
     }
 	
     class OperationButton implements ActionListener {
-	public void actionPerformed(ActionEvent event) {
-	    nextNum = Double.parseDouble(numHolder);
-	    numHolder = "0";
-	    button = event.getActionCommand();
-	    result = evaluator.evaluate(result, nextNum, assign);
-	
-	    if (button == " + ") {
-		displayBox.setText(displayBox.getText() + button);
-		assign = 'a';
-	    } else if (button == " - ") {
-		displayBox.setText(displayBox.getText() + button);
-		assign = 's';
-	    } else if (button == " * ") {
-		displayBox.setText(displayBox.getText() + button);
-		assign = 'm';
-	    } else if (button == " / ") {
-		displayBox.setText(displayBox.getText() + button);
-		assign = 'd';
-	    } else if (button == "sin(x)") {
-		displayBox.setText(displayBox.getText() + "sin(");
-		assign = 'S';	
-	    } else if (button == "cos(x)") {
-		displayBox.setText(displayBox.getText() + "cos(");
-		assign = 'C';	
-	    } else if (button == "tan(x)") {
-		displayBox.setText(displayBox.getText() + "tan(");
-		assign = 'T';	
-	    } else if (button == "sqrt") {
-		displayBox.setText(displayBox.getText() + "sqrt(");
-		assign = 'r';				
-	    } else 
-		assign = 'n';	
-	}
+        public void actionPerformed(ActionEvent event) {
+            num = Double.parseDouble(numHolder);
+            numHolder = "0";
+            button = event.getActionCommand();
+            
+            if (button == " + "|| button == " - "|| button == " * "|| button == " / "|| button == " ^ "){
+                displayBox.setText(displayBox.getText() + button);
+                input = input + button;
+            } else if (button == "sqrt"){
+                displayBox.setText(displayBox.getText() + "sqrt(");
+                squareroot = true;
+            } else if (button == "("){
+                displayBox.setText(displayBox.getText() + button);
+                input = input + "( "; //extra space makes string readable to OrderOfOperations class
+            } else if (button == "^"){
+                displayBox.setText(displayBox.getText() + button);
+                input = input + " ^ ";
+            } else if (button == ")"){
+                if (squareroot){
+                    num = Math.sqrt(num);
+                    input = input + Double.toString(num);
+                    squareroot = false;
+                }else{
+                    input = input + " )";
+                }
+                displayBox.setText(displayBox.getText() + button);
+            } else{
+                displayBox.setText("");
+                input = "";
+            }
+        }
     }
-
+    
     class EqualsButton implements ActionListener {
-	public void actionPerformed(ActionEvent event) {
-	    nextNum = Double.parseDouble(numHolder);	
-	    numHolder = "0";
-	    button = event.getActionCommand();
-	    result = evaluator.evaluate(result, nextNum, assign);
-	    displayBox.setText(displayBox.getText() + button + formater.format(result));	
-	}
-    }
-	
-    class ClearButton implements ActionListener {
-	public void actionPerformed(ActionEvent event) {
-	    displayBox.setText("");
-	    numHolder = "0";
-	    first = true;
-	}
+    	public void actionPerformed(ActionEvent event){
+    		button = event.getActionCommand();
+    		result = evaluator.readInput(input);
+    		displayBox.setText(displayBox.getText() + button + formater.format(result));
+    	}
     }
 }
-
-
